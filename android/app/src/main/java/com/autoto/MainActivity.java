ead76cb783371c8af3c2956cbc402cb6832638e0
Aviv <vaknins33@gmail.com>
2020-04-04 23:01:15 +0300

    changed com.fuelType to com.autoto

--- android/app/src/main/java/com/autoto/MainActivity.java
+++ android/app/src/main/java/com/autoto/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fueltype;
+package com.autoto;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "fuelType";
+    return "autoto";
   }
 }
--- android/app/src/main/java/com/autoto/MainApplication.java
+++ android/app/src/main/java/com/autoto/MainApplication.java
@@ -1,9 +1,10 @@
-package com.fueltype;
+package com.autoto;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -62,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fueltype.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.autoto.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
